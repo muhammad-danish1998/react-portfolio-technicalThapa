63701bd428f2b589f75dd6c47d95e8cdf1090f3b
muhammad-danish1998 <iamdanish1998@gmail.com>
2020-09-21 11:32:00 +0500

    first commit

---
+++ src/Sdata.js
@@ -0,0 +1,34 @@
+import a11  from './images/a11.jpg'
+import a12  from './images/a12.png'
+import a13  from './images/a13.jpg'
+import a14  from './images/a14.jpg'
+import a15  from './images/a15.jpg'
+
+
+const Sdata = [
+    {
+        imgSrc : a11,
+        title : 'Web Development'
+    },
+    {
+        imgSrc : a12,
+        title : 'Mobile Development'
+    },
+    {
+        imgSrc : a13,
+        title : 'IOS Development'
+    },
+    {
+        imgSrc : a14,
+        title : 'Android  Development'
+    },
+    {
+        imgSrc : a15,
+        title : 'SEO Development'
+    },
+    {
+        imgSrc : a12,
+        title : 'Hyberd Application Development'
+    },
+]
+export default Sdata;
